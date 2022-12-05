c73b18877f8093a0ba9feaa55ab7e6fa26aa3f48
seoyeon <wjddowkd456@naver.com>
2022-12-05 22:52:05 +0900

    221205

--- 044.js
+++ 044.js
@@ -1,24 +1,12 @@
-// 영어가 싫어요
-function solution(numbers) {
-    const obj = {
-        zero: 0,
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9,
-    };
+// 약수 구하기
+function solution(n) {
+    var answer = [];
 
-    const num = numbers.replace(
-        /zero|one|two|three|four|five|six|seven|eight|nine/g,
-        (v) => {
-            return obj[v];
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            answer.push(i);
         }
-    );
+    }
 
-    return Number(num);
+    return answer;
 }
