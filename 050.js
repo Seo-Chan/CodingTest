c1f825701c46875ffd1eb8035d53b023fb3c5fdc
seoyeon <wjddowkd456@naver.com>
2022-12-17 18:59:36 +0900

    221217

---
+++ 050.js
@@ -0,0 +1,26 @@
+// 영어가 싫어요
+// https://school.programmers.co.kr/learn/courses/30/lessons/120894
+
+function solution(numbers) {
+  const obj = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  const num = numbers.replace(
+    /zero|one|two|three|four|five|six|seven|eight|nine/g,
+    (v) => {
+      return obj[v];
+    }
+  );
+
+  return Number(num);
+}
