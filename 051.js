29dd013a509bff9fc578d6ebf21dbe587d46b012
seoyeon <wjddowkd456@naver.com>
2022-12-18 23:10:08 +0900

    221218

---
+++ 051.js
@@ -0,0 +1,13 @@
+// 배열 회전시키기
+// https://school.programmers.co.kr/learn/courses/30/lessons/120844
+
+function solution(numbers, direction) {
+  var answer = [];
+  if ("right" == direction) {
+    numbers.unshift(numbers.pop());
+  } else {
+    numbers.push(numbers.shift());
+  }
+  answer = numbers;
+  return answer;
+}
