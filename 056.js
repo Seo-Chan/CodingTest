a51abfcc05bc432e6c5cd674e8608eaca04349e5
seoyeon <wjddowkd456@naver.com>
2023-01-04 23:38:01 +0900

    230104

---
+++ 056.js
@@ -0,0 +1,7 @@
+// 삼각형의 완성조건1
+// https://school.programmers.co.kr/learn/courses/30/lessons/120889
+
+function solution(sides) {
+  정렬 = sides.sort((a, b) => a - b);
+  return 정렬[2] < 정렬[0] + 정렬[1] ? 1 : 2;
+}
